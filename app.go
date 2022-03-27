26f0fccd9804466845959980195ba6fb2e8bba0a
chihuo2104 <chihuo2104@moekonnyaku.com>
2022-03-27 15:55:36 +0800

    chore: tidy

--- app.go
+++ app.go
@@ -1,6 +1,8 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
--- routes/index.go
+++ routes/index.go
@@ -1,15 +1,1 @@
 package routes_index
-
-import (
-	"log"
-
-	"github.com/gin-gonic/gin"
-)
-
-func route(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"status": "200",
-		"hello":  "World!",
-	})
-	log.Println("Requested at /")
-}
