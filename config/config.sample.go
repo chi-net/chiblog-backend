13a1d845c8fc5cada05120dc28678eca1563a46d
chihuo2104 <chihuo2104@moekonnyaku.com>
2022-04-03 20:17:07 +0800

    feat: config sample

---
+++ config/config.sample.go
@@ -0,0 +1,19 @@
+package config
+
+// 服务监听端口
+var ServiceListenPort = 3000
+
+// 数据库名
+var DatabaseName = "chiapp"
+
+// 数据库端口
+var DatabasePort = 3306
+
+// 数据库host
+var DatabaseHost = "127.0.0.1"
+
+// 数据库密码
+var DatabasePassword = "chihuo2104"
+
+// 数据库用户名
+var DatabaseUserName = "root"
