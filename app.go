982919cfea8804715b5fc162918b87d165dd98a4
chihuo2104 <chihuo2104@moekonnyaku.com>
2022-04-03 16:37:34 +0800

    fix: error

--- app.go
+++ app.go
@@ -1,14 +1,23 @@
+/*
+ * @Author: chihuo2104
+ * @Date: 2022-04-03 16:27:09
+ * @LastEditTime: 2022-04-03 16:35:29
+ * @LastEditors: chihuo2104
+ * @Description: go app file
+ * @FilePath: \chiblog-backend\app.go
+ * Powered by chihuo2104<chihuo2104@moekonnyaku.com>.All right reserved ©2018-now.
+ */
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
 	"github.com/chihuo2104/chiblog-backend/routes"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", routes.Index())
+	app.Get("/", routes.Index)
 
 	app.Listen(":3000")
 }
--- routes/index.go
+++ routes/index.go
@@ -1,9 +1,18 @@
+/*
+ * @Author: chihuo2104
+ * @Date: 2022-04-03 16:27:09
+ * @LastEditTime: 2022-04-03 16:35:56
+ * @LastEditors: chihuo2104
+ * @Description: routes-index
+ * @FilePath: \chiblog-backend\routes\index.go
+ * Powered by chihuo2104<chihuo2104@moekonnyaku.com>.All right reserved ©2018-now.
+ */
 package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func Index (c *fiber.Ctx) {
-	return c.SendString("Hello, World 👋!")
+func Index(c *fiber.Ctx) error {
+	return c.SendString("What a stupid taromaru it is!")
 }
